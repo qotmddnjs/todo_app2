7b70511edf9d733f557dd5224e19c33886e8195f
qotmddnjs <dudcnsaus@gmail.com>
2024-04-01 16:57:02 +0900

    tailwind, mui 충돌 피하는 방법

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+'use client';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <>
-    <div></div>
+      <Button className="tw-bg-red-500" variant="contained">
+        Contained
+      </Button>
     </>
-  
   );
 }
