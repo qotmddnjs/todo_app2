8760c1add33d41978fbe72d581d74c5bc05179e3
qotmddnjs <dudcnsaus@gmail.com>
2024-04-02 16:48:30 +0900

    dateToStr 모듈화, App에 적용, theme를 App에서 분리, 공통 테마 적용

--- src/app/page.js
+++ src/app/page.js
@@ -2,45 +2,204 @@
 
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, AppBar, Toolbar, Tabs, Tab } from '@mui/material';
-import theme from './theme';
+import { Button, AppBar, Toolbar, CssBaseline } from '@mui/material';
 import { FaBars } from 'react-icons/fa';
-import Link from 'next/link';
+import theme from './theme';
+import dateToStr from './dateUtil';
 
-export default function App() {
-  const [tabCurrentIndex, setTabCurrentIndex] = React.useState(0);
+const useTodoStatus = () => {
+  const [todos, setTodos] = React.useState([]);
+  const lastTodoIdRef = React.useRef(0);
+
+  const addTodo = (newTitle) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      title: newTitle,
+      regDate: dateToStr(new Date()),
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id != id);
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (id, title) => {
+    const newTodos = todos.map((todo) => (todo.id != id ? todo : { ...todo, title }));
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    removeTodo,
+    modifyTodo,
+  };
+};
+
+const NewTodoForm = ({ todoStatus }) => {
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+
+  const addTodo = () => {
+    if (newTodoTitle.trim().length == 0) return;
+    const title = newTodoTitle.trim();
+    todoStatusaddTodo(title);
+    setNewTodoTitle('');
+  };
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <div className="tw-flex-1">
-              <FaBars onClick={() => setOpen(true)} className="tw-cursor-pointer" />
-            </div>
-            <div className="logo-box">
-              <a href="/" className="tw-font-bold">
-                NOTE!
-              </a>
-            </div>
-            <div className="tw-flex-1 tw-flex tw-justify-end">
-              <a href="/write">글쓰기</a>
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-        <section className="tw-h-screen tw-flex tw-items-center tw-justify-center tw-text-[5rem]">
-          section
-        </section>
-      </ThemeProvider>
-      <Tabs value={tabCurrentIndex} onChange={(_, newValue) => setTabCurrentIndex(newValue)}>
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
-      </Tabs>
-      {tabCurrentIndex == 0 && <div>내용1</div>}
-      {tabCurrentIndex == 1 && <div>내용2</div>}
-      {tabCurrentIndex == 2 && <div>내용3</div>}
+      <div className="flex items-center gap-x-3">
+        <input
+          className="input input-bordered"
+          type="text"
+          placeholder="새 할일 입력해"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={addTodo}>
+          할 일 추가
+        </button>
+      </div>
     </>
+  );
+};
+
+const TodoListItem = ({ todo, todoStatus }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [newTodoTitle, setNewTodoTitle] = useState(todo.title);
+  const readMode = !editMode;
+
+  const enableEditMode = () => {
+    setEditMode(true);
+  };
+
+  const removeTodo = () => {
+    todoStatus.removeTodo(todo.id);
+  };
+
+  const cancleEdit = () => {
+    setEditMode(false);
+    setNewTodoTitle(todo.title);
+  };
+  const commitEdit = () => {
+    if (newTodoTitle.trim().length == 0) return;
+
+    todoStatus.modifyTodo(todo.id, newTodoTitle.trim());
+
+    setEditMode(false);
+  };
+
+  return (
+    <li className="flex items-center gap-x-3 mb-3">
+      <span className="badge badge-accent badge-outline">{todo.id}</span>
+      {readMode ? (
+        <>
+          <span>{todo.title}</span>
+          <button className="btn btn-outline btn-accent" onClick={enableEditMode}>
+            수정
+          </button>
+          <button className="btn btn-accent" onClick={removeTodo}>
+            삭제
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            className="input input-bordered"
+            type="text"
+            placeholder="할 일 써"
+            value={newTodoTitle}
+            onChange={(e) => setNewTodoTitle(e.target.value)}
+          />
+          <button className="btn btn-accent" onClick={commitEdit}>
+            수정완료
+          </button>
+          <button className="btn btn-accent" onClick={cancleEdit}>
+            수정취소
+          </button>
+        </>
+      )}
+    </li>
+  );
+};
+
+const TodoList = ({ todoStatus }) => {
+  return (
+    <>
+      {todoStatus.todos.length == 0 ? (
+        <h4>할 일 없음</h4>
+      ) : (
+        <>
+          <h4>할 일 목록</h4>
+          <ul>
+            {todoStatus.todos.map((todo) => (
+              <TodoListItem key={todo.id} todo={todo} todoStatus={todoStatus} />
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+};
+
+const App = () => {
+  const todoState = useTodoStatus(); // 리액트 커스텀 훅
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+
+    form.title.value = form.title.value.trim();
+
+    if (form.title.value.length == 0) {
+      alert('할 일 써');
+      form.title.focus();
+      return;
+    }
+
+    todoState.addTodo(form.title.value);
+    form.title.value = '';
+    form.title.focus();
+  };
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="tw-flex-1">
+            <FaBars onClick={() => setOpen(true)} className="tw-cursor-pointer" />
+          </div>
+          <div className="logo-box">
+            <a href="/" className="tw-font-bold">
+              TODO!
+            </a>
+          </div>
+          <div className="tw-flex-1 tw-flex tw-justify-end">
+            <a href="/write">글쓰기</a>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <form onSubmit={onSubmit}>
+        <input type="text" name="title" autoComplete="off" placeholder="할 일 입력해" />
+        <button type="submit">추가</button>
+        <button type="reset">취소</button>
+      </form>
+      {todoState.todos.length}
+    </>
+  );
+};
+
+export default function themeApp() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   );
 }
